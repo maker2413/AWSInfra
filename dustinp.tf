64daf3134d202819b2ff8eb5469f075b59713b04
maker2413 <maker2413@gmail.com>
2022-10-06 16:55:56 -0500

    converted dustinp to new static-site module

--- dustinp.tf
+++ dustinp.tf
@@ -1,57 +1,5 @@
 # --- dustinp.tf ---
 
-module "dustinp_iam" {
-  source = "./modules/iam/"
-
-  username    = "dustinp-service-account"
-
-  tags = {
-    Project = "Notes"
-    Repo    = "github.com/kirigaine/dustinp.me-website"
-  }
-}
-
-resource "aws_iam_access_key" "dustinp_access_key" {
-  user = module.dustinp_iam.user.name
-}
-
-resource "aws_iam_user_policy" "dustinp_s3_policy" {
-  name = "dustinp-service-account-s3-policy"
-  user = module.dustinp_iam.user.name
-
-  policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Sid": "ObjectLevel",
-      "Effect": "Allow",
-      "Action": [
-        "s3:PutObject",
-        "s3:GetObject",
-        "s3:DeleteObject"
-      ],
-      "Resource": "arn:aws:s3:::www.dustinp.me/*"
-    }
-  ]
-}
-EOF
-}
-
-module "dustinp_s3" {
-  source = "./modules/s3"
-
-  acl            = "public-read"
-  bucket_name    = "www.dustinp.me"
-  index_document = "index.html"
-
-  tags = {
-    Name    = "www.dustinp.me"
-    Project = "Notes"
-    Repo    = "github.com/kirigaine/dustinp.me-website"
-  }
-}
-
 module "dustinp_acm" {
   source = "./modules/acm"
 
@@ -66,16 +14,15 @@ module "dustinp_acm" {
   }
 }
 
-module "dustinp_cloudfront" {
-  source = "./modules/cloudfront"
+module "dustinp" {
+  source = "./modules/static-site"
 
-  acm_arn     = module.dustinp_acm.acm_arn
-  aliases     = ["dustinp.me", "www.dustinp.me"]
-  domain_name = module.dustinp_s3.bucket_regional_domain_name
+  acm_arn            = module.dustinp_acm.acm_arn
+  cloudfront_aliases = ["dustinp.me", "www.dustinp.me"]
+  domain             = "www.dustinp.me"
 
   tags = {
-    Name    = "dustinp.me"
-    Project = "Notes"
+    Project = "Website"
     Repo    = "github.com/kirigaine/dustinp.me-website"
   }
 }
